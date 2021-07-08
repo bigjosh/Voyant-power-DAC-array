6cc34fec37f846a9c952dfc6cabfe90e44fd0f9c
josh <github@junk.josh.com>
2021-07-08 02:16:59 -0400

    Initial commit
    Readme with mods, software for test patterns.

---
+++ Arduino/DACDemo/DACDemo.ino
@@ -0,0 +1,113 @@
+/////////////////////////////////////////////////////////////////////////////////
+//Sine Table Lookup
+//This program creates a sine wave using the sin function
+///2014 Visgence Inc
+/////////////////////////////////////////////////////////////////////////////////
+#include <SPI.h>
+#define SS1 (1 << 2) //Slave Select 1 PORTB
+#define SS1PORT &PORTB   
+
+#define SS2 (1 << 1) //Slave Select 2 PORTB
+#define SS2PORT &PORTB
+
+#define SS3 (1 << 0) //Slave Select 3 PORTB
+#define SS3PORT &PORTB
+
+#define SS4 (1 << 7) //Slave Select 4 PORTD
+#define SS4PORT &PORTD
+
+const int ss1 = 10;
+const int ss2 = 9;
+const int ss3 = 8;
+const int ss4 = 7;
+
+void setup() {
+  // set the slaveSelectPins as an output:
+  pinMode (ss1, OUTPUT);
+  pinMode (ss2, OUTPUT);
+  pinMode (ss3, OUTPUT);
+  pinMode (ss4, OUTPUT);
+
+  // initialize SPI:
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE0);
+}
+
+const unsigned dac_steps = 4096;// (1<<12);   // 12 bits of resolution
+
+void loop() {
+
+  
+  for(unsigned i=0;i<dac_steps;i++) {
+    
+    writeMCP492x(i, SS1, SS1PORT);
+
+  }
+
+
+  for(unsigned i=0;i<dac_steps;i+=315) {    // 315 is a factor of 4095
+    
+    writeMCP492x(i , SS1, SS1PORT);
+    _delay_us(2200);
+
+  }
+  
+
+  for(float a = 0 ; a < 2 * 3.1415 ; a+=0.02) {
+   
+    unsigned value = (unsigned)(
+      sin( a ) * (dac_steps/2) + (dac_steps/2)
+    );
+    
+    writeMCP492x(value , SS1, SS1PORT);
+
+  }
+         
+  
+}
+
+
+
+
+//Method to write to the DAC,using  digitalWrite for slave select
+void writeMCP492x(uint16_t data,uint8_t slave_select) {
+  // Take the top 4 bits of config and the top 4 valid bits (data is actually a 12 bit number) 
+  //and OR them together
+  uint8_t top_msg = (0x30 & 0xF0) | (0x0F & (data >> 8));
+
+  // Take the bottom octet of data
+  uint8_t lower_msg = (data & 0x00FF);
+
+  // Select our DAC, Active LOW
+  digitalWrite(slave_select, LOW);
+
+  // Send first 8 bits
+  SPI.transfer(top_msg);
+  // Send second 8 bits
+  SPI.transfer(lower_msg);
+
+  //Deselect DAC
+  digitalWrite(slave_select, HIGH);
+}
+
+
+//Method to write to the DAC, using direct port for slave select
+void writeMCP492x(uint16_t data,uint8_t ss,volatile uint8_t* slave_port) {
+  // Take the top 4 bits of config and the top 4 valid bits (data is actually a 12 bit number) 
+  //and OR them together
+  uint8_t top_msg = (0x30 & 0xF0) | (0x0F & (data >> 8));
+
+  // Take the bottom octet of data
+  uint8_t lower_msg = (data & 0x00FF);
+
+  // Select our DAC, Active LOW
+  *slave_port &= ~ss;
+
+  // Send first 8 bits
+  SPI.transfer(top_msg);
+  // Send second 8 bits
+  SPI.transfer(lower_msg);
+
+  //Deselect DAC
+  *slave_port |= ss;
+}
